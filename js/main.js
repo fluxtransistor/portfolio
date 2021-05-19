a31ef6a7b0ef7db947e9036f7aff0a07567411ae
fluxtransistor <lukipav@gmail.com>
2021-05-19 09:32:14 +0200

    Add very basic typing animation

---
+++ js/main.js
@@ -0,0 +1,29 @@
+async function type() {
+  var text =
+   ["$ whoami","","",
+    "Hi, I'm Lukáš!","",
+    "This is a placeholder for some hopefully interesting stuff."
+   ];
+  document.getElementById("terminal-text").innerHTML = "";
+  await sleep(1200);
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == "") {
+      document.getElementById("terminal-text").innerHTML += "<br/>";
+    } else {
+      for (var j = 0; j < text[i].length; j++) {
+        await sleep(100);
+        document.getElementById("terminal-text").innerHTML += text[i][j];
+      }
+    }
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener('readystatechange', function() {
+  if (document.readyState === "complete") {
+    type();
+  }
+})
